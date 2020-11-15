8e583ae5b37c03cbc971ed15b8336ec0f7833644
umemiya0715 <whiteblue116@icloud.com>
2020-11-16 01:30:02 +0900

    model_spec 01

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :task do
-    
+    title "First task"
+    status "todo" 
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+   
+    it 'is valid with all attributes' do
+      task = build(:task)
+      expect(task).to be_valid  
+    end  
+ 
+    it 'is invalid without title' do
+      task = build(:task, title: nil)
+      expect(task).to be_invalid
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it 'is invalid without status' do
+      task = build(:task, status: nil)
+      expect(task).to be_invalid
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+ 
+    it 'is invalid with a duplicate title' do
+      task = build(:task)
+      new_task = build(:task, title: 'First Title')
+      new_task.valid?
+      expect(task.errors[:title]).to include("has already been taken")   
+    end
+ 
+    it 'is valid with another title' do
+      task = build(:task)
+      new_task = build(:task, title: 'Second Title')
+      expect(new_task).to be_valid
+    end
+  end
 end
