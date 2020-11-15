3294be32befd019f0f7c04f2706918945c50f7db
umemiya0715 <whiteblue116@icloud.com>
2020-11-16 01:50:12 +0900

    Model Spec 01 Fixed

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :task do
-    title "First task"
-    status "todo" 
+    sequence(:title, "title_1")
+    content { "content" }
+    status { :todo } 
+    deadline { 1.week.from_now }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,32 +5,34 @@ RSpec.describe Task, type: :model do
    
     it 'is valid with all attributes' do
       task = build(:task)
-      expect(task).to be_valid  
+      expect(task).to be_valid
+      expect(task.errors).to be_empty  
     end  
  
     it 'is invalid without title' do
-      task = build(:task, title: nil)
-      expect(task).to be_invalid
-      expect(task.errors[:title]).to include("can't be blank")
+      task_without_title = build(:task, title: "")
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.errors[:title]).to eq["can't be blank]
     end
 
     it 'is invalid without status' do
-      task = build(:task, status: nil)
-      expect(task).to be_invalid
-      expect(task.errors[:status]).to include("can't be blank")
+      task_without_status = build(:task, status: nil)
+      expect(task_without_status).to be_invalid
+      expect(task_without_status.errors[:status]).to eq("can't be blank")
     end
  
     it 'is invalid with a duplicate title' do
-      task = build(:task)
-      new_task = build(:task, title: 'First Title')
-      new_task.valid?
-      expect(task.errors[:title]).to include("has already been taken")   
+      task = create(:task)
+      task_with_duplicated_title = build(:task, title: task.title)
+      expect(task_with_duplicated_titile).to be_invalid
+      expect(task_with_duplicated_title.errors[:title]).to eq("has already been taken")   
     end
  
     it 'is valid with another title' do
-      task = build(:task)
-      new_task = build(:task, title: 'Second Title')
-      expect(new_task).to be_valid
+      task = create(:task)
+      task_with_another_title = build(:task, title: 'Second Title')
+      expect(task_with_another_title).to be_valid
+      expect(task_with_anothwr_title.errors).to be_empty
     end
   end
 end
